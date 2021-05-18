cd82164859b1be0026d5cf26059c5c21392cec2d
cruz911 <joelchadambuka@gmail.com>
2021-05-18 23:09:07 +0200

    jfjfj

--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -36,6 +36,23 @@ def get_all_users():
 def get_a_user(public_id):
     return User.query.filter_by(public_id=public_id).first()
 
+def generate_token(user):
+    try:
+        # generate the auth token
+        auth_token = user.encode_auth_token(user.id)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully registered.',
+            'Authorization': auth_token.decode()
+        }
+        return response_object, 201
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': 'Some error occurred. Please try again.'
+        }
+        return response_object, 401
+
 
 def save_changes(data):
     db.session.add(data)
